9c682ada3820141f27e4f8991c450b30ccfe8013
Royce Beryl Laput <royce.laput@gmail.com>
2025-04-18 22:51:51 +1200

    initial commit

---
+++ servicesLeadTime.js
@@ -0,0 +1,29 @@
+const servicesLeadTime = [
+  {
+    CustomerType: "AccountHolder",
+    Services: {
+      LaserCutting: 4,
+      LaserCuttingAndFolding: 5,
+      LaserCuttingAndMachineShop: 6,
+      LaserCuttingAndFoldingAndMachineShop: 7,
+    },
+  },
+  {
+    CustomerType: "CashSale",
+    Services: {
+      LaserCutting: 5,
+      LaserCuttingAndFolding: 6,
+      LaserCuttingAndMachineShop: 7,
+      LaserCuttingAndFoldingAndMachineShop: 8,
+    },
+  },
+  {
+    CustomerType: "Top15",
+    Services: {
+      LaserCutting: 3,
+      LaserCuttingAndFolding: 4,
+      LaserCuttingAndMachineShop: 5,
+      LaserCuttingAndFoldingAndMachineShop: 6,
+    },
+  },
+];

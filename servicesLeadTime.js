7845ced6209eb4cf901389e1cd77d2a8fdc4900c
Royce Laput <royce.laput@fisherpaykel.com>
2025-07-18 09:14:36 +1200

    update lead times

--- servicesLeadTime.js
+++ servicesLeadTime.js
@@ -1,20 +1,20 @@
 const servicesLeadTime = {
   "Account Holder": [
-    { name: "Cutting", days: 4 },
-    { name: "Cutting And Folding", days: 9 },
-    { name: "Cutting And MachineShop", days: 8 },
-    { name: "Cutting And Folding And MachineShop", days: 12 },
+    { name: "Cutting", days: 3 },
+    { name: "Cutting And Folding", days: 6 },
+    { name: "Cutting And MachineShop", days: 5 },
+    { name: "Cutting And Folding And MachineShop", days: 7 },
   ],
   "Cash Sale": [
-    { name: "Cutting", days: 7 },
-    { name: "Cutting And Folding", days: 15 },
-    { name: "Cutting And MachineShop", days: 12 },
-    { name: "Cutting And Folding And MachineShop", days: 17 },
+    { name: "Cutting", days: 3 },
+    { name: "Cutting And Folding", days: 6 },
+    { name: "Cutting And MachineShop", days: 5 },
+    { name: "Cutting And Folding And MachineShop", days: 8 },
   ],
   "Top 15": [
     { name: "Cutting", days: 3 },
-    { name: "Cutting And Folding", days: 5 },
+    { name: "Cutting And Folding", days: 4 },
     { name: "Cutting And MachineShop", days: 4 },
-    { name: "Cutting And Folding And MachineShop", days: 7 },
+    { name: "Cutting And Folding And MachineShop", days: 5 },
   ],
 };

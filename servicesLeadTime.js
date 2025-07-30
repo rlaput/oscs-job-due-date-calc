69f2a2ad661391fa77763885d4e5717a1d1fb6b3
Royce Beryl Laput <royce.laput@gmail.com>
2025-07-31 08:19:01 +1200

    add deliver by calculation

--- servicesLeadTime.js
+++ servicesLeadTime.js
@@ -1,20 +1,20 @@
 const servicesLeadTime = {
   "Account Holder": [
-    { name: "Cutting", days: 3 },
-    { name: "Cutting And Folding", days: 6 },
-    { name: "Cutting And MachineShop", days: 5 },
-    { name: "Cutting And Folding And MachineShop", days: 7 },
+    { name: "Cutting", productionDays: 2, deliveryDays: 1 },
+    { name: "Cutting And Folding", productionDays: 4, deliveryDays: 1 },
+    { name: "Cutting And MachineShop", productionDays: 4, deliveryDays: 1 },
+    { name: "Cutting And Folding And MachineShop", productionDays: 5, deliveryDays: 1 },
   ],
   "Cash Sale": [
-    { name: "Cutting", days: 3 },
-    { name: "Cutting And Folding", days: 6 },
-    { name: "Cutting And MachineShop", days: 5 },
-    { name: "Cutting And Folding And MachineShop", days: 8 },
+    { name: "Cutting", productionDays: 2, deliveryDays: 1 },
+    { name: "Cutting And Folding", productionDays: 4, deliveryDays: 1 },
+    { name: "Cutting And MachineShop", productionDays: 4, deliveryDays: 1 },
+    { name: "Cutting And Folding And MachineShop", productionDays: 5, deliveryDays: 1 },
   ],
   "Top 15": [
-    { name: "Cutting", days: 3 },
-    { name: "Cutting And Folding", days: 4 },
-    { name: "Cutting And MachineShop", days: 4 },
-    { name: "Cutting And Folding And MachineShop", days: 5 },
+    { name: "Cutting", productionDays: 2, deliveryDays: 1 },
+    { name: "Cutting And Folding", productionDays: 4, deliveryDays: 1 },
+    { name: "Cutting And MachineShop", productionDays: 4, deliveryDays: 1 },
+    { name: "Cutting And Folding And MachineShop", productionDays: 5, deliveryDays: 1 },
   ],
 };

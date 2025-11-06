a99765d6afd7fb0c45500aec13d07be8af9373bc
Royce Beryl Laput <royce.laput@gmail.com>
2025-11-07 09:04:31 +1300

    add tube cutting service

--- servicesLeadTime.js
+++ servicesLeadTime.js
@@ -4,17 +4,20 @@ const servicesLeadTime = {
     { name: "Cutting And Folding", productionDays: 4, deliveryDays: 1 },
     { name: "Cutting And MachineShop", productionDays: 4, deliveryDays: 1 },
     { name: "Cutting And Folding And MachineShop", productionDays: 5, deliveryDays: 1 },
+    { name: "Tube Cutting", productionDays: 5, deliveryDays: 1 },
   ],
   "Cash Sale": [
     { name: "Cutting", productionDays: 2, deliveryDays: 1 },
     { name: "Cutting And Folding", productionDays: 4, deliveryDays: 1 },
     { name: "Cutting And MachineShop", productionDays: 4, deliveryDays: 1 },
     { name: "Cutting And Folding And MachineShop", productionDays: 5, deliveryDays: 1 },
+    { name: "Tube Cutting", productionDays: 5, deliveryDays: 1 },
   ],
   "Top 15": [
     { name: "Cutting", productionDays: 2, deliveryDays: 1 },
     { name: "Cutting And Folding", productionDays: 4, deliveryDays: 1 },
     { name: "Cutting And MachineShop", productionDays: 4, deliveryDays: 1 },
     { name: "Cutting And Folding And MachineShop", productionDays: 5, deliveryDays: 1 },
+    { name: "Tube Cutting", productionDays: 5, deliveryDays: 1 },
   ],
 };

82919de2c1c1bd8842bf57521dd33fd2043db788
Royce Laput <royce.laput@gmail.com>
2025-11-10 09:58:32 +1300

    Update production days for service options

--- servicesLeadTime.js
+++ servicesLeadTime.js
@@ -1,16 +1,16 @@
 const servicesLeadTime = {
   "Account Holder": [
     { name: "Cutting", productionDays: 2, deliveryDays: 1 },
-    { name: "Cutting And Folding", productionDays: 4, deliveryDays: 1 },
-    { name: "Cutting And MachineShop", productionDays: 4, deliveryDays: 1 },
-    { name: "Cutting And Folding And MachineShop", productionDays: 5, deliveryDays: 1 },
+    { name: "Cutting And Folding", productionDays: 6, deliveryDays: 1 },
+    { name: "Cutting And MachineShop", productionDays: 6, deliveryDays: 1 },
+    { name: "Cutting And Folding And MachineShop", productionDays: 8, deliveryDays: 1 },
     { name: "Tube Cutting", productionDays: 4, deliveryDays: 1 },
   ],
   "Cash Sale": [
     { name: "Cutting", productionDays: 2, deliveryDays: 1 },
-    { name: "Cutting And Folding", productionDays: 4, deliveryDays: 1 },
-    { name: "Cutting And MachineShop", productionDays: 4, deliveryDays: 1 },
-    { name: "Cutting And Folding And MachineShop", productionDays: 5, deliveryDays: 1 },
+    { name: "Cutting And Folding", productionDays: 6, deliveryDays: 1 },
+    { name: "Cutting And MachineShop", productionDays: 6, deliveryDays: 1 },
+    { name: "Cutting And Folding And MachineShop", productionDays: 8, deliveryDays: 1 },
     { name: "Tube Cutting", productionDays: 4, deliveryDays: 1 },
   ],
   "Top 15": [
